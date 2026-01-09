33d0712594b8a2527e1abacfbb1a69603cee8763
rdyb87 <rcsadib@gmail.com>
2026-01-09 16:05:06 +0800

    Adjust remove hardcode port 5000

--- app.py
+++ app.py
@@ -353,8 +353,8 @@ def create_admin():
     print(f'Admin user {username} created successfully.')
 
 
-if __name__ == '__main__':
-    # Ensure DB is created
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host="0.0.0.0", port=port)
